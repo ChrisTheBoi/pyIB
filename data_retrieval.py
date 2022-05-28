92fc4d4f3c5933917014ad181044199ab226714c
ChrisTheBoi <36931354+ChrisTheBoi@users.noreply.github.com>
2022-05-27 23:50:57 -0400

    added function to find CIK then used said function to modify sec-edgar-api functions

--- data_retrieval.py
+++ data_retrieval.py
@@ -4,9 +4,11 @@ import urllib.request as url_r
 from user_agent import generate_user_agent
 import re
 from sec_edgar_api import EdgarClient
+import random
 
 random_ua = generate_user_agent()
 edgar = EdgarClient(user_agent=random_ua)
+
 
 def get_cik(ticker):
     """function to retrieve CIK of company"""
@@ -22,12 +24,20 @@ def get_cik(ticker):
 
     return cik
 
+
 def get_submissions_(ticker):
     cik_num = get_cik(ticker)
     request = edgar.get_submissions(cik=cik_num)
     return request
 
-def get_company_concept_(ticker,tag,taxonomy='us-gaap'):
+
+def get_company_concept_(ticker, tag, taxonomy='us-gaap'):
     cik_num = get_cik(ticker)
-    request = edgar.get_company_concept(cik=cik_num,taxonomy=taxonomy,tag=tag)
+    request = edgar.get_company_concept(cik=cik_num, taxonomy=taxonomy, tag=tag)
+    return request
+
+
+def get_company_facts_(ticker):
+    cik_num = get_cik(ticker)
+    request = edgar.get_company_facts(cik=cik_num)
     return request
